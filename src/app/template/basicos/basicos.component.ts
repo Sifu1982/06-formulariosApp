789cfe62f1ddfd704be2bfd769ebbc033a31fefd
David Martínez Trigo <davidmtrigo@hotmail.es>
2021-10-07 20:13:52 +0200

    feat: template Básicos form working with two validations

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class BasicosComponent implements OnInit {
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  nombreValido(): boolean {
+    return (
+      this.miFormulario?.controls.producto?.invalid &&
+      this.miFormulario?.controls.producto.touched
+    );
+  }
+
+  precioValido(): boolean {
+    return (
+      this.miFormulario?.controls.precio?.touched &&
+      this.miFormulario?.controls.precio.value < 0
+    );
+  }
+
+  // guardar(miFormulario: NgForm) {
+  guardar() {
+    console.log(this.miFormulario);
+  }
 }
