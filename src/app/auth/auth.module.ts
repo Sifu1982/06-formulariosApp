3bd6e89424a85e0792d9065dc56549a0a11e351d
David Martínez Trigo <davidmtrigo@hotmail.es>
2021-10-18 18:02:29 +0200

    feat: custom validations and validator.service

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { RegistroComponent } from './pages/registro/registro.component';
-import { LoginComponent } from './pages/login/login.component';
 
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 @NgModule({
-  declarations: [
-    RegistroComponent,
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [RegistroComponent, LoginComponent],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,16 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import {
+  nombreApellidoPattern,
+  emailPattern,
+  noPuedeSerStrider,
+} from '../../../shared/validator/validaciones';
+import { ValidatorService } from 'src/app/shared/validator/validator.service';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RegistroComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group(
+    {
+      nombre: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorService.nombreApellidoPattern),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorService.emailPattern),
+        ],
+      ],
+      // Cuando se use una validación personalizada, al llamar al método, recordar no usar ()
+      userName: [
+        '',
+        [Validators.required, this.validatorService.noPuedeSerStrider],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required]],
+    },
+    {
+      validators: [
+        this.validatorService.camposIguales('password', 'password2'),
+      ],
+    }
+  );
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private validatorService: ValidatorService
+  ) {}
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'David Martinez',
+      email: 'test1@test.com',
+      userName: 'Sifu1982',
+    });
   }
 
+  campoNoValido(campo: string) {
+    return (
+      this.miFormulario.get(campo)?.invalid &&
+      this.miFormulario.get(campo)?.touched
+    );
+  }
+
+  submitFormulario() {
+    console.log(this.miFormulario.value);
+
+    // Esto dispara las validaciones en cada uno de los campos
+    this.miFormulario.markAllAsTouched();
+  }
 }
