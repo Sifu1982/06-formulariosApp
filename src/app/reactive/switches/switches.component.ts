74043dc07fc5789d62e47b15d214ca0429327dc7
David Martínez Trigo <davidmtrigo@hotmail.es>
2021-10-18 13:10:00 +0200

    feat: created auth.module and children paths. Layout of registro.component

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,16 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SwitchesComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue],
+  });
 
-  constructor() { }
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  };
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      ...this.persona,
+      condiciones: false,
+    });
+
+    this.miFormulario.valueChanges.subscribe(({ condiciones, ...rest }) => {
+      // Mediante desestructuración extraigo condiciones y todas las demás propiedades quedan en el argumento rest
+      this.persona = rest;
+    });
   }
 
+  guardar() {
+    const formValue = { ...this.miFormulario.value };
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+  }
 }
